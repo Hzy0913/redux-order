bc86181e6531b61606e2a98a35b9a3ae23ff31e0
Hzy0913 <zhaoyun.han@kongming-inc.com>
2018-04-11 15:07:11 +0800

    feat(es5):   兼容es5写法

--- index.js
+++ index.js
@@ -1,28 +1,39 @@
-export default function reduxOrder() {
-  return ({dispatch, getState}) => next => (action) => {
-    if (typeof action === 'function') {
-      return action(dispatch, getState);
-    }
-    const {types = [], promise} = action;
-    const [REQUEST, SUCCESS, FAIL] = types;
-    function orderAction(type, resultType, result) {
-      const state = {type, [resultType]: result};
-      Object.keys(action).forEach(item => {
-        if (item === 'types' || item === 'promise' || item === 'type') return;
-        state[item] = action[item];
-      });
-      return state;
-    }
-    if (!promise) {
-      return next(action);
-    }
-    dispatch(orderAction(REQUEST));
-    return action.promise.then(res => {
-      if (!SUCCESS) return;
-      return dispatch(orderAction(SUCCESS, 'res', res));
-    }).catch((err) => {
-      if (!FAIL) return;
-      return dispatch(orderAction(FAIL, 'err', err));
-    });
+function reduxOrder() {
+  return function(reduxAction) {
+    return function(next) {
+      return function(action) {
+        var dispatch = reduxAction.dispatch;
+        var getState = reduxAction.getState;
+        if (typeof action === 'function') {
+          return action(dispatch, getState);
+        }
+        var types = (action || {}).types || [];
+        var promise = (action || {}).promise || null;
+        var REQUEST = types[0];
+        var SUCCESS = types[1];
+        var FAIL = types[2];
+        function orderAction(type, resultType, result) {
+          var state = {type: type};
+          state[resultType] = result;
+          for (var item in action) {
+            if (item === 'types' || item === 'promise' || item === 'type') break;
+            state[item] = action[item];
+          };
+          return state;
+        };
+        if (!promise) {
+          return next(action);
+        }
+        dispatch(orderAction(REQUEST));
+        return action.promise.then(function(res) {
+          if (!SUCCESS) return;
+          return dispatch(orderAction(SUCCESS, 'res', res));
+        }).catch(function(err) {
+          if (!FAIL) return;
+          return dispatch(orderAction(FAIL, 'err', err));
+        });
+      };
+    };
   };
-}
+};
+export default reduxOrder;
